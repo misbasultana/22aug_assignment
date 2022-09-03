05a84a40acc78fa4310fe95e2d98ebd859152788
Misba Sultana <94856283+misbasultana@users.noreply.github.com>
2022-09-03 10:56:38 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+
+function App() {
+  return (
+    <section>
+      <h2> About Me</h2>
+      <h4>Contact Details</h4>
+      <ul> 
+        <li>👩‍🎓Misba Sultana </li>
+        <li>📧misbasultana5555@gmail.com</li>
+        <li>📞9206444208</li>
+        <li>📍#12, R Krishnappa layout, Bhuvaneshwari Nagar, Bangalore - 560032</li>
+      </ul>
+
+      <h4>Personal Details</h4>
+      <ul> 
+        <li>Date of Birth : 22/03/2001 </li>
+        <li>Marital Status : Single</li>
+        <li>Nationality : Indian</li>
+      </ul>
+
+      <h4>Objective</h4>
+      <p>I seek challenging opportunities where I can fully use my skills for the success of the organization.</p>
+
+      <h4>Interests</h4>
+      <ul> 
+        <li>Art & Craft </li>
+        <li>Books</li>
+        <li>Travelling</li>
+        <li>Social Work</li>
+      </ul>
+
+      <h4>Languages Known</h4>
+      <ul> 
+        <li>English</li>
+        <li>Hindi</li>
+        <li>Kannada</li>
+      </ul>
+    </section>
+  );
+}
+
+export default App;
